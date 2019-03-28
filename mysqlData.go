0e2d728b2aa7cacc66fdf3c11add17b4e16757da
Harold Schreckengost <harold@haroldmschreckengost.com>
2019-03-28 11:43:54 -0500

    fmt

--- mysqlData.go
+++ mysqlData.go
@@ -3,16 +3,16 @@ package main
 import "time"
 
 type MySQLProcess struct {
-	ID       *int    `json:"ID"`
-	User     *string `json:"User"`
-	Host     *string `json:"Host"`
-	Database *string `json:"Database"`
-	Command  *string `json:"Command"`
-	Time     *int    `json:"Time"`
-	State    *string `json:"State"`
-	Info     *string `json:"Info"`
-	Date time.Time `json:"Date"`
-	DatabaseHost string `json:"DatabaseHost"`
+	ID           *int      `json:"ID"`
+	User         *string   `json:"User"`
+	Host         *string   `json:"Host"`
+	Database     *string   `json:"Database"`
+	Command      *string   `json:"Command"`
+	Time         *int      `json:"Time"`
+	State        *string   `json:"State"`
+	Info         *string   `json:"Info"`
+	Date         time.Time `json:"Date"`
+	DatabaseHost string    `json:"DatabaseHost"`
 }
 
 type MySQLProcessList struct {
